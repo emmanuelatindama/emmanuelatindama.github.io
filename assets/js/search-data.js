c6d5767958ce909a2873bb29d384184b4d9c8b39
emmanuelatindama <emmanuelatindama@gmail.com>
2025-04-25 19:27:40 +0000

    Deploying to gh-pages from @ emmanuelatindama/emmanuelatindama.github.io@c010c739fe00f5143e59a27a01a37c08bbfd09b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,17 +55,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/2025-04-22-image-restoration/";
           
         },
-      },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/2018-12-22-distill/";
-          
-        },
       },{id: "post-model-comparison",
         
           title: "Model Comparison",
@@ -130,6 +119,17 @@ ninja.data = [{
         handler: () => {
           
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+          
+        },
+      },{id: "post-test-total-variation-for-restoration-of-ebsd-maps",
+        
+          title: "Test - Total Variation for Restoration of EBSD Maps",
+        
+        description: "In this blog, I decribe a Total Variation model for denoising microscopy data for materials. It is an old model that has been repurposed and proposed for this work.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/distill/";
           
         },
       },{id: "news-a-simple-inline-announcement",
