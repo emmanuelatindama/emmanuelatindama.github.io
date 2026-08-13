0f853660d651cab7165cc03ad66587a087fb6570
emmanuelatindama <emmanuelatindama@gmail.com>
2026-08-13 03:53:20 +0000

    Deploying to gh-pages from @ emmanuelatindama/emmanuelatindama.github.io@27ff097f984e306a949263872ebc790fe4f68c3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -127,6 +127,11 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-exploring-probability",
+          title: 'exploring probability',
+          description: "when expected value lies to you — interactive simulations of multiplicative betting games",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
